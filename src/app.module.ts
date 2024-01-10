3789a7974fa6d43ebfc7dfce6845bb41812ec050
Mateus Belli <mateusbelli@hotmail.com>
2024-01-10 20:58:20 -0300

    Configurações inciiais postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ProductsModule],
+  imports: [
+    UserModule,
+    ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
